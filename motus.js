05ca98863b6bd3b66dc3be113803070af2b17569
gaetan <courtemanche.gaetan@gmail.com>
2024-04-18 17:56:16 +0200

    test3

--- motus.js
+++ motus.js
@@ -14,6 +14,18 @@ const data = require("./bibliotheque.json");
 const colors = require("colors");
 const player = require("play-sound")();
 
+//inputText est la fenêtre de dialogue readline qui permet au joueur de deviner un mot
+const inputText = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Life nous permet de choisir un nombre d'essai maximum
+// On décrémentera ce nombre d'essai avec remainingLives
+const life = process.argv[2];
+let remainingLives = life;
+
+// fonction sleep qui quand elle est appelée ajoute un délai (elle sert principalement pour délair chaque tour de boucle au moment de vérifier les lettres )
 const sleep = (milliseconds) => {
   const start = new Date().getTime();
   for (let i = 0; i < 1e7; i++) {
@@ -22,6 +34,11 @@ const sleep = (milliseconds) => {
     }
   }
 };
+
+// guessWord est le mot de base que l'on veut deviner et mot est celui que le joueur entre dans la fenêtre de dialogue.
+// la fonction isSame vérifie d'abord si la longueur de guessWord et mot sont les mêmme. Ensuite elle boucle sur chaque lettres pour vérifier si elle sont pareils
+// Elle affiche en rouge les lettres bien placés, en jaunes les lettres mal placés, un X si la lettre n'est pas dans guessWord
+// Petite précision : au départ cette fonction était tout en bas du code mais je l'ai replacé en haut pour plus facilement l'appeler ensuite.
 const isSame = (a, b) => {
   if (a.length !== b.length) {
     return (
@@ -34,6 +51,7 @@ const isSame = (a, b) => {
       })
     );
   }
+  // Ici j'ai créé une variable qui m'aide à définir des conditions ensuite.
   let isCharSame = true;
   for (let i = 0; i < a.length; i++) {
     sleep(6000);
@@ -67,10 +85,12 @@ const isSame = (a, b) => {
   }
 };
 
+// Ici on filtre le tableau de donnée pour n'afficher que des mots de moins de 6 lettres.
 datafilter = data.filter((item) => item.label.length <= 5);
 const TabData = datafilter[Math.floor(Math.random() * datafilter.length)];
 const guessWord = TabData.label;
 
+// cette fonction cache les lettres du mot à deviner sauf le premier, j'en ai profiter pour ajouter de la decoration
 function hideLetters(guessWord) {
   let hideWord = guessWord[0];
   for (let i = 1; i < guessWord.length; i++) {
@@ -93,19 +113,13 @@ function hideLetters(guessWord) {
 }
 console.log(hideLetters(guessWord));
 
-const inputText = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const life = process.argv[2];
-let remainingLives = life;
-
+// la fonction playMotus permet au joueur de deviner un mot, elle s'appelle elle à chaque éssai. j'utilise la fonction sleep pour que les étapes du jeu soient plus claires
 const playMotus = (mot) => {
   sleep(2000, console.log("\nvous avez essayé le mot : " + mot));
 
   isSame(guessWord, mot);
 
+  // la fonction guess peremet de savoir si le le joueur a déviner le mot, elle décrémente le nombre d'essai en cas d'échec.
   const guess = () => {
     if (guessWord === mot) {
       console.log(`
