6cffa0a42ae68e9daa8073dbbf8746c9a3d459a7
gaetan <courtemanche.gaetan@gmail.com>
2024-04-18 15:15:10 +0200

    first commit

---
+++ motus.js
@@ -0,0 +1,137 @@
+// Pour faire fonctionner le jeu, initlialisez npm et  installez les packages nécessaires :
+// npm install
+// npm install play-sounds colors readline
+// Pour déclencher le jeu entrez node motus.js et le nombre d'essai que vous voulez. Par exemple :
+// node motus.js 5
+// N'oubliez pas d'activer le son de votre device et amusez-vous bien !
+
+const readline = require("readline");
+const data = require("./bibliotheque.json");
+const colors = require("colors");
+const player = require("play-sound")();
+
+const sleep = (milliseconds) => {
+  const start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+};
+const isSame = (a, b) => {
+  if (a.length !== b.length) {
+    return (
+      "\nC'est perdu ! =(",
+      player.play("./assets/sounds/lose.wav", (err) => {
+        if (err) {
+          console.error("Impossible de jouer le son:", err);
+        }
+      }),
+      inputText.close(sleep(3000))
+    );
+  } else {
+    let isCharSame = true;
+    for (let i = 0; i < a.length; i++) {
+      sleep(6000);
+      if (a[i] !== b[i]) {
+        isCharSame = false;
+      } else {
+        isCharSame = true;
+      }
+      if (isCharSame === true) {
+        process.stdout.write(colors.red(a[i]));
+        player.play("./assets/sounds/valid.wav", (err) => {
+          if (err) {
+            console.error("Impossible de jouer le son:", err);
+          }
+        });
+      } else if (a.includes(b[i])) {
+        process.stdout.write(colors.yellow(b[i]));
+        player.play("./assets/sounds/misplaced.wav", (err) => {
+          if (err) {
+            console.error("Impossible de jouer le son:", err);
+          }
+        });
+      } else {
+        process.stdout.write("X");
+        player.play("./assets/sounds/wrong.wav", (err) => {
+          if (err) {
+            console.error("Impossible de jouer le son:", err);
+          }
+        });
+      }
+    }
+  }
+};
+
+datafilter = data.filter((item) => item.label.length <= 5);
+const TabData = datafilter[Math.floor(Math.random() * datafilter.length)];
+const guessWord = TabData.label;
+
+function hideLetters(guessWord) {
+  let hideWord = guessWord[0];
+  for (let i = 1; i < guessWord.length; i++) {
+    hideWord += "*";
+  }
+  return "Mot mystère : " + hideWord;
+}
+console.log(hideLetters(guessWord));
+
+const inputText = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const life = process.argv[2];
+let remainingLives = life;
+
+const playMotus = (mot) => {
+  console.log("\nvous avez essayé le mot : " + mot);
+
+  isSame(guessWord, mot);
+
+  const guess = () => {
+    if (guessWord === mot) {
+      console.log(`
+      \nBravo ! Vous avez deviné le mot ${mot} `);
+      player.play("./assets/sounds/victorycut.mp4", (err) => {
+        if (err) {
+          console.error("Impossible de jouer le son:", err);
+        }
+      });
+      inputText.close(sleep(2000));
+    } else {
+      if (remainingLives != 0) {
+        remainingLives--;
+        console.log(`
+        \nRaté ! Il vous reste ${remainingLives} essais`);
+      }
+      if (remainingLives === 0 || guessWord.length !== mot.length)
+        return (
+          console.log("\nC'est perdu ! =("),
+          player.play("./assets/sounds/lose.wav", (err) => {
+            if (err) {
+              console.error("Impossible de jouer le son:", err);
+            }
+          }),
+          inputText.close(sleep(3000))
+        );
+    }
+  };
+
+  guess();
+
+  if (remainingLives != 0) {
+    inputText.question(
+      `\nveuillez entrer un mot de ${guessWord.length} lettres : `,
+      playMotus,
+      sleep(1000)
+    );
+  }
+};
+if (remainingLives != 0) {
+  inputText.question(
+    `\nveuillez entrer un mot de ${guessWord.length} lettres : `,
+    playMotus
+  );
+}
